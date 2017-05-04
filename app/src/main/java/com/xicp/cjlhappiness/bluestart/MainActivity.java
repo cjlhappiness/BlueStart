2722cdc03b5c4985e37b43b4a85b976ec045a793
cjlhappiness <570604900@qq.com>
2017-05-04 13:37:16 +0800

    First commit.

--- app/src/main/java/com/xicp/cjlhappiness/bluestart/MainActivity.java
+++ app/src/main/java/com/xicp/cjlhappiness/bluestart/MainActivity.java
@@ -93,17 +93,21 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_item_1) {
 
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_item_2) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_item_3) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_item_4) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_item_5) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_item_6) {
+
+        }else if (id == R.id.nav_item_7) {
+
+        }else if (id == R.id.nav_item_8) {
 
         }
 
